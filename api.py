07854da6ac0035b811d9c98f7ecece4c92790a43
Sebas Moya <sebas0163@estudiantec.cr>
2023-09-21 23:51:38 -0600

    [Add] Creacion de investigadores

--- api.py
+++ api.py
@@ -2,22 +2,23 @@ from flask import Flask, jsonify,render_template, redirect, request
 from neo4j import GraphDatabase
 
 #conexion con la base de datos
+#IMPORTANTE: Agregar datos de la base que se va a usar
 driver = GraphDatabase.driver(uri='bolt://localhost:7687',auth=('neo4j','12345678'))
 session=driver.session()
 
 api = Flask(__name__) 
 
-#rutas
+#Funciones de POBLACION DE LA BASE
 
-@api.route('/create/<string:nombre>&<int:id>', methods = ['GET', 'POST'])
-def Create_node(nombre, id):
-    query ="create (n:Employee{NAME:$nombre,ID:$id})"
-    map_ ={"nombre":nombre, "id":id}
+@api.route('/cargar_invest/<string:nombre>&<string:titulo>&<string:institucion>&<string:email>', methods = ['GET', 'POST'])
+def Create_node(nombre, titulo, institucion, email):
+    query ="create (i:Investigador{Nombre:$nombre,Titulo:$titulo, Institucion:$institucion, Email:$email})"
+    map_ ={"nombre":nombre, "titulo":titulo, "institucion":institucion, "email":email}
     try:
         session.run(query,map_)
-        return (f"empleado creado")
+        return ("Investigador creado con exito")
     except Exception as e:
-        return("error")
+        return(str(e))
 if __name__ == "__main__":
     api.run(port=5050)
 """
