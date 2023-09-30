8a80de21187c31f886d7011d5e740dfa2eef3c95
Wajib Zaglul <wajib10@gmail.com>
2023-09-30 13:44:43 -0600

    [edit] Language

--- static/javascript/top_researchers.js
+++ static/javascript/top_researchers.js
@@ -20,9 +20,9 @@ socket.on("topResearchersAPI/get", (data) => {
         institution.textContent = element.institucion;
         fila.appendChild(institution);
 
-        const proyectsQuantity = document.createElement("td");
-        proyectsQuantity.textContent = element.cantidad_proyectos
-        fila.appendChild(proyectsQuantity);
+        const projectsQuantity = document.createElement("td");
+        projectsQuantity.textContent = element.cantidad_proyectos
+        fila.appendChild(projectsQuantity);
 
         tbody.appendChild(fila);
     });
